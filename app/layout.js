d5223ed24f6ff019156b2b1c8c693b3de4e2bf53
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2024-06-08 03:05:40 +0530

    feat: create header, improve layout

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,7 @@
 import { Josefin_Sans } from "next/font/google";
 
 import "@/app/_styles/globals.css";
-
-const josefin = Josefin_Sans({
-    subsets: ["latin"],
-    display: "swap"
-})
+import Header from "./_components/Header";
 
 export const metadata = {
     title: {
@@ -16,11 +12,23 @@ export const metadata = {
         "A luxurious cabin hotel located in the heart of the Italian Dolomites, surrounded by beautiful mountains and lush green forests",
 };
 
+const josefin = Josefin_Sans({
+    subsets: ["latin"],
+    display: "swap",
+});
+
+const today = new Date();
+
 export default function RootLayout({ children }) {
     return (
         <html lang="en">
-            <body className={`${josefin.className} bg-primary-950 text-primary-100 min-h-screen`}>
-                {children}
+            <body
+                className={`${josefin.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}
+            >
+                <Header />
+                <div className="flex-1 px-8 py-12">
+                    <main className="max-w-7xl mx-auto">{children}</main>
+                </div>
             </body>
         </html>
     );
