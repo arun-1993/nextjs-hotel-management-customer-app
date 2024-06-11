f9f0fbe0340ff311fba8158033c9385b0b1dbb68
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2024-06-11 19:48:03 +0530

    feat: use React Suspense to display static content while fetching data

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,18 +1,19 @@
-import CabinCard from "../_components/CabinCard";
-import { getCabins } from "../_utils/data-service";
+import { Suspense } from "react";
+
+import CabinList from "../_components/CabinList";
+import Loading from "./loading";
 
 export const metadata = {
     title: "Cabins",
 };
 
-export default async function Page() {
-    const cabins = await getCabins();
-
+export default function Page() {
     return (
         <div>
             <h1 className="text-4xl mb-5 text-accent-400 font-medium">
                 Our Luxury Cabins
             </h1>
+
             <p className="text-primary-200 text-lg mb-10">
                 Cozy yet luxurious cabins, located right in the heart of the
                 Italian Dolomites. Imagine waking up to beautiful mountain
@@ -22,13 +23,9 @@ export default async function Page() {
                 perfect spot for a peaceful, calm vacation. Welcome to paradise.
             </p>
 
-            {cabins.length > 0 && (
-                <div className="grid sm:grid-cols-1 md:grid-cols-2 gap-8 lg:gap-12 xl:gap-14">
-                    {cabins.map((cabin) => (
-                        <CabinCard cabin={cabin} key={cabin.id} />
-                    ))}
-                </div>
-            )}
+            <Suspense fallback={<Loading />}>
+                <CabinList />
+            </Suspense>
         </div>
     );
 }
