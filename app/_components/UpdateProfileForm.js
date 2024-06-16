7246d623c00caa1191fc703507e74fed52c54410
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2024-06-17 02:52:49 +0530

    feat: add visual indicator to show profile data is being updated

--- app/_components/UpdateProfileForm.js
+++ app/_components/UpdateProfileForm.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { updateProfileAction } from "../_utils/actions";
+import UpdateButton from "./UpdateButton";
 
 export default function UpdateProfileForm({ guest, children }) {
     const { fullName, email, nationalID, countryFlag } = guest;
@@ -52,9 +53,7 @@ export default function UpdateProfileForm({ guest, children }) {
             </div>
 
             <div className="flex justify-end items-center gap-6">
-                <button className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300">
-                    Update profile
-                </button>
+                <UpdateButton />
             </div>
         </form>
     );
