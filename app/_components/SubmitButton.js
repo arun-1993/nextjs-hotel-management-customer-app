014031abcd97da46d789dda5f34c770862cfd97a
Arun Ravindran <39104424+arun-1993@users.noreply.github.com>
2024-06-20 21:39:37 +0530

    feat: change UpdateButton to SubmitButton and make it more reusable

--- app/_components/SubmitButton.js
+++ app/_components/SubmitButton.js
@@ -1,6 +1,8 @@
+"use client";
+
 import { useFormStatus } from "react-dom";
 
-export default function UpdateButton() {
+export default function SubmitButton({ buttonText, busyText = null }) {
     const { pending } = useFormStatus();
 
     return (
@@ -8,7 +10,7 @@ export default function UpdateButton() {
             className="bg-accent-500 px-8 py-4 text-primary-800 font-semibold hover:bg-accent-600 transition-all disabled:cursor-not-allowed disabled:bg-gray-500 disabled:text-gray-300"
             disabled={pending}
         >
-            {pending ? "Updating..." : "Update profile"}
+            {pending ? busyText ?? buttonText : buttonText}
         </button>
     );
 }
